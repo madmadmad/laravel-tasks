cb105bcae2bc5070f3405651ee0c0ab97ae871a6
smockensturm <steve@madmadmad.com>
2023-07-25 15:44:26 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Task List
+                    Madhouse Task List
                 </a>
             </div>
 
